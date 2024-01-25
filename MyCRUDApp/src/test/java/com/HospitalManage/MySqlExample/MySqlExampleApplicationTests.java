611e58378b4782bffcd3ce30f1dd927ee596992c
JatsVangani <57982136+JatsVangani@users.noreply.github.com>
2024-01-25 23:04:49 +0530

    Updated class names

--- MyCRUDApp/src/test/java/com/HospitalManage/MySqlExample/MySqlExampleApplicationTests.java
+++ MyCRUDApp/src/test/java/com/HospitalManage/MySqlExample/MySqlExampleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.HospitalManage.MySqlExample;
+package com.HospitalManage.MyCRUDApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MySqlExampleApplicationTests {
+class MyCRUDAppTests {
 
 	@Test
 	void contextLoads() {
