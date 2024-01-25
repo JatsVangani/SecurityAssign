ffb363395d00909467f8dc503403129f75426303
JatsVangani <57982136+JatsVangani@users.noreply.github.com>
2024-01-25 23:00:46 +0530

    Updating the code

--- MyCRUDApp/src/main/java/com/HospitalManage/MyCRUDApp/MySqlExampleApplication.java
+++ MyCRUDApp/src/main/java/com/HospitalManage/MyCRUDApp/MySqlExampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySqlExampleApplication {
+public class MyCRUDApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySqlExampleApplication.class, args);
